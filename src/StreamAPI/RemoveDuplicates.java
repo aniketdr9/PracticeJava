a8292dabfb276e0d5aab17ee2824df86fe78956b
Aniket Rathod <57947853+aniketdr9@users.noreply.github.com>
2024-03-23 22:21:26 +0530

    remove duplicates using stream

---
+++ src/StreamAPI/RemoveDuplicates.java
@@ -0,0 +1,13 @@
+package StreamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RemoveDuplicates {
+    public static void main(String[] args){
+        List<Integer> numbers = Arrays.asList(1,2,3,4,4,5,6,6,7,7,7,8,9,10,11);
+        List<Integer> distinctNumber = numbers.stream().distinct().collect(Collectors.toList());
+        System.out.print(distinctNumber);
+    }
+}
