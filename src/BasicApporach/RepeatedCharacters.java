1787bdacfe77f4154e0fceb413b311407e3e3d6c
Aniket Rathod <57947853+aniketdr9@users.noreply.github.com>
2024-03-31 22:26:08 +0530

    repeated characters in a string

---
+++ src/BasicApporach/RepeatedCharacters.java
@@ -0,0 +1,32 @@
+package BasicApporach;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class RepeatedCharacters {
+    public static String findRepeatedCharacters(String inputString){
+        inputString = inputString.toLowerCase(Locale.ROOT);
+        int[] charFreq = new int[256];
+        for (int i = 0; i< inputString.length();i++){
+            char ch = inputString.charAt(i);
+            if (Character.isLetter(ch)){
+                charFreq[ch]++;
+            }
+        }
+
+        System.out.print("Repeated characters in String: ");
+        for (int i =0;i<charFreq.length; i++){
+            if(charFreq[i] > 1 && Character.isLetter((char) i)){
+                System.out.println((char) i + " - " + charFreq[i] + " times.");
+            }
+        }
+        return inputString;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        findRepeatedCharacters(str);
+    }
+}
