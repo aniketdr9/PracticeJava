f1e1d054a860ad15f0dbe723dd72bb5472f777b0
Aniket Rathod <57947853+aniketdr9@users.noreply.github.com>
2024-04-02 16:00:38 +0530

    all possible unique subset from an array

---
+++ src/LeetCode/Medium/ArraySubset.java
@@ -0,0 +1,34 @@
+package LeetCode.Medium;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ArraySubset {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of an array: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for(int i=0; i < size; i++) {
+            System.out.print("Enter " + (i + 1) + " element: ");
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("All possible subset: "+subsets(arr));
+    }
+
+    public static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> resultList = new ArrayList<>();
+        backtrack(resultList, new ArrayList<>(), nums, 0);
+        return resultList;
+    }
+
+    private static void backtrack(List<List<Integer>> resultSets, List<Integer> tempSet, int[] nums, int start){
+        resultSets.add(new ArrayList<>(tempSet));
+        for(int i =start; i< nums.length; i++){
+            tempSet.add(nums[i]);
+            backtrack(resultSets, tempSet, nums, i+1);
+            tempSet.remove(tempSet.size() - 1);
+        }
+    }
+}
